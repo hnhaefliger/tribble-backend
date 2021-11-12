dbda3a50a9a0c173510060c3fff00eecc1db4a62
hnhaefliger <henry_haefliger@protonmail.com>
2021-11-11 20:56:04 -0500

    allow all cors

--- tribble/tribble/settings.py
+++ tribble/tribble/settings.py
@@ -39,11 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
+
     'rest_framework',
-    'browser_sessions'
+
+    'browser_sessions',
+    'text_analysis',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
