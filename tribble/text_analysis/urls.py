93b39ace48e41949761133c5d667725ed32c9a45
hnhaefliger <henry_haefliger@protonmail.com>
2021-11-19 09:45:54 -0500

    minor dependency change

--- tribble/text_analysis/urls.py
+++ tribble/text_analysis/urls.py
@@ -5,5 +5,6 @@ router = routers.DefaultRouter()
 
 router.register('language', LanguageViewSet, basename='language')
 router.register('keywords', KeywordViewSet, basename='keywords')
+router.register('summary', SummaryViewSet, basename='summary')
 
 urlpatterns = router.urls
--- tribble/text_analysis/views.py
+++ tribble/text_analysis/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
 from .cortical import detect_language, detect_keywords
+from .summary import generate_summary
 
 
 class LanguageViewSet(viewsets.ViewSet):
@@ -19,3 +20,18 @@ class KeywordViewSet(viewsets.ViewSet):
 
     def create(self, request, *args, **kwargs):
         return Response(data=detect_keywords(request.data['text']), status=status.HTTP_200_OK)
+
+
+class SummaryViewSet(viewsets.ViewSet):
+    permission_classes = (AllowAny,)
+    lookup_url_kwarg = 'language'
+
+    def create(self, request, *args, **kwargs):
+        data = '<p>Keywords:    ' + '    '.join(detect_keywords(request.data['text'])) + '</p>'
+        summary = generate_summary(request.data['text'])
+        
+        if summary != '':
+            data += '<p>' + summary + '</p>'
+
+
+        return Response(data=data, status=status.HTTP_200_OK)
